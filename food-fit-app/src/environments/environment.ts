a690720ae64e969e4bc9d8655e58fd6ec7379ca2
Luis Eduardo <luiseperezp.10@gmail.com>
2025-11-03 18:04:19 -0500

    feat: add environment configuration files for Firebase

---
+++ food-fit-app/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "XXXXX-XXXXXXXXX-XXXXXX-XXXXXX",
+        authDomain: "XXXXXXXXXXXX.firebaseapp.com",
+        projectId: "xxxxxxxxxxxx",
+        storageBucket: "XXXXXXXXXXXX.appspot.com",
+        messagingSenderId: "XXXXXXXXXXXX",
+        appId: "1:XXXXXXXXXXXX:web:XXXXXXXXXXXX"
+    },
+    modelGemini: 'gemini-2.5-flash-lite',
+    modelImageGemini: '	imagen-4.0-generate-001'
+};
