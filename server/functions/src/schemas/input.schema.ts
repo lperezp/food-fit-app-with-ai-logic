a67b9ae82870ac49fc9548f6ff7d053948ce9d87
Luis Eduardo <luiseperezp.10@gmail.com>
2025-11-06 17:25:45 -0500

    Remove server functions

--- server/functions/src/schemas/input.schema.ts
+++
@@ -1,6 +0,0 @@
-import { z } from 'genkit';
-
-export const inputSchema = z.object({
-    ingredient: z.string(),
-    quantity_people: z.number()
-});
--- server/functions/src/schemas/output-list-food-item.schema.ts
+++
@@ -1,6 +0,0 @@
-import { outputFoodItemSchema } from './output-food-item.schema';
-import { z } from 'genkit';
-
-export const outputListFoodItemSchema = z.object({
-    recipes: z.array(outputFoodItemSchema)
-});
